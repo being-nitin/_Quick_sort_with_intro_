7555ba6304ea7f63bb836c916a085d71141d1c0b
Nitin <nsinghboss02@gmail.com>
2022-03-28 23:46:59 +0530

    "Quick sort complete code"

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -4,7 +4,35 @@ public class Main {
 
     public static void main(String[] args) {
 	/*
-	Quick sort with explaination
+	 Quick sort with explanation
 	 */
+        int[] arr = {7,9,1,3,5,0,2,45,21,14};
+        quick(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void quick(int[] arr, int low,int high){
+        if(low>=high){
+            return;
+        }
+        int start = low;
+        int end = high;
+        int pivot = start + (end-start)/2;
+        while(start<=end){
+            while(arr[start]<arr[pivot]){
+                start++;
+            }
+            while (arr[end]>arr[pivot]){
+                end--;
+            }
+            if(start<=end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        quick(arr,low,end);
+        quick(arr,start,high);
     }
 }
